f663a8a622f4a022d552ad65fbb97265b2ebac70
Roshitha8129 <guderoshitha9@gmail.com>
2026-05-06 18:29:31 +0530

    commiting the final change

--- frontend/frontend/backend/app/services/pdf_parser.py
+++
@@ -1,8 +0,0 @@
-from pypdf import PdfReader
-
-def extract_text(file_path):
-    reader = PdfReader(file_path)
-    text = ""
-    for page in reader.pages:
-        text += page.extract_text()
-    return text
--- frontend/frontend/backend/app/services/vector_store.py
+++
@@ -1,7 +0,0 @@
-from langchain_community.vectorstores import FAISS
-from langchain_community.embeddings import HuggingFaceEmbeddings
-
-def create_vector_store(text_chunks):
-    embeddings = HuggingFaceEmbeddings()
-    db = FAISS.from_texts(text_chunks, embeddings)
-    return db
